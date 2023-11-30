1f2b466a9bcbca83dfae6e3f58d292a0f80bc83b
shaco-star <sherifm846@gmail.com>
2023-11-30 12:04:08 -0800

    commit

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,8 +2,8 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    this.amount = amount;
+    this.currency = currency;
   }
 
   set amount(amount) {
