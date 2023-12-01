c71144090ebb7eccad3f38597d108394079eb372
shaco-star <sherifm846@gmail.com>
2023-12-01 03:35:19 -0800

    commit

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (const [idx, value] of array.entries()) {
+    // eslint-disable-next-line no-param-reassign
+    array[idx] = appendString + value;
+  }
+  return array;
+}
