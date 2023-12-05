9e68b4708e40fd5edc4ba3a5d8c16b4cddb73c20
shaco-star <sherifm846@gmail.com>
2023-12-05 00:54:56 -0800

    commit

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  let result = '';
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      result += `${item.slice(startString.length)}-`;
+    }
+  }
+  return result.slice(0, -1);
+}
