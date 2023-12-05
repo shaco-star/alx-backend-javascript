04ccc07dbe8e951ef7e3bd70f141763735ab1872
shaco-star <sherifm846@gmail.com>
2023-12-05 11:20:07 -0800

    fixed

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
-    throw new Error('Position outside range');
+    return new Error('Position outside range');
   }
-  const buffer = new DataView(new ArrayBuffer(length), 0, length);
-  buffer.setInt8(position, value);
+  const buffer = new ArrayBuffer(length);
+  const dv = new DataView(buffer, 0, length);
+
+  dv.setInt8(position, value);
+
   return buffer;
 }
