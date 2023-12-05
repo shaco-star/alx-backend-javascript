a156b07bb962d86c3a83c90a26f82a93010bf213
shaco-star <sherifm846@gmail.com>
2023-12-05 11:13:59 -0800

    fixed

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,5 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position > length) {
+  if (position >= length) {
     return new Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
