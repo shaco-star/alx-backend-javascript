70c4f9bb75dd3cc0f9125e55f9b18702c3a90687
shaco-star <sherifm846@gmail.com>
2024-01-29 11:16:57 -0800

    commit

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n')
+
+process.stdin.on("data", data => {
+    process.stdout.write('Your name is: '+ data)
+})
+
+
+process.on("exit", () =>{
+    process.stdout.write('This important software is now closing\n');
+});
